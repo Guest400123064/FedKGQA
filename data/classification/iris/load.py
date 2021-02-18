a4b22da08060d44f3d92052a3416227b794baf6a
Guest400123064 <lgfz1003@icloud.com>
2021-02-18 18:35:05 +0800

    .

---
+++ data/classification/iris/load.py
@@ -0,0 +1,16 @@
+import torch
+import pandas as pd
+import numpy as np
+
+from torch.utils.data import Dataset
+
+
+class IrisDataset(Dataset):
+
+    def __init__(self, path_data="data.csv"):
+
+        return
+
+
+if __name__ == "__main__":
+    pass
--- src/intro_torch/logistic_regression/model.py
+++ src/intro_torch/logistic_regression/model.py
@@ -6,13 +6,15 @@ import torch.nn.functional as F
 class LogisticRegression(nn.Module):
 
     def __init__(self, factor_dim, target_dim):
+        
         super().__init__()
         self.linear = nn.Linear(factor_dim, target_dim)
         return
 
     def forward(self, x):
-        lin = self.linear(x)
-        return F.log_softmax(lin, dim=-1)
+        
+        linear = self.linear(x)
+        return F.log_softmax(linear, dim=-1)
 
 
 if __name__ == "__main__":
