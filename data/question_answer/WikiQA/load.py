34a2d90142aa91d626b1d00697be5cfad3ac9630
Guest400123064 <lgfz1003@icloud.com>
2021-03-01 10:38:54 +0800

    toxic comments dataset

---
+++ data/question_answer/WikiQA/load.py
@@ -0,0 +1,28 @@
+import os
+import torch
+
+from torchtext.data.dataset import TabularDataset
+
+
+class WikiQADataset(TabularDataset):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+        return
+
+    def __len__(self):
+
+        return
+
+    def __getitem__(self, idx):
+        
+        return
+
+
+class WikiQADataLoader:
+
+    def __init__(self) -> None:
+        pass
