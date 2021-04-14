8788e79e33ea7af47ff427ffc5165aef490ede34
Guest400123064 <lgfz1003@icloud.com>
2021-04-14 23:17:13 +0800

    redo toxic comment data loader

--- data/classification/toxic_comments/load.py
+++ data/classification/toxic_comments/load.py
@@ -5,38 +5,32 @@ import torch
 
 import numpy as np
 import pandas as pd
+from collections import Counter
+from easydict import EasyDict
 
+from torchtext.vocab import Vectors, Vocab
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import (
     Dataset
     , DataLoader
-    , random_split
+    , SubsetRandomSampler
 )
 
 
 class ToxicComtVocab(object):
 
-    def __init__(self, freq_bar=5):
-
+    def __init__(self, min_freq=5) -> None:
+        
         self.is_built = False
-        self.freq_bar = freq_bar
+        self.min_freq = min_freq
+        self.vocab = None
         self.tokenizer = spacy.load("en_core_web_sm")
-        self.itos = {
-            0: "<PAD>"    # Padding
-            , 1: "<SOS>"  # Start of Sentence
-            , 2: "<EOS>"  # End of Sentence
-            , 3: "<UNK>"  # Unknown
-        }
-        self.stoi = {
-            "<PAD>": 0
-            , "<SOS>": 1
-            , "<EOS>": 2
-            , "<UNK>": 3
-        }
         return
 
     def __len__(self):
-        return len(self.itos)
+
+        ret = -1 if (not self.is_built) else len(self.vocab)
+        return ret
 
     def tokenize(self, text, max_num_char=2000):
 
@@ -56,19 +50,15 @@ class ToxicComtVocab(object):
 
     def build_vocab(self, list_texts):
 
-        freq_stat, idx = {}, 4  # start from idx4; idx 3 is <UNK>
+        freq = Counter()
         for text in list_texts:
-            for token in self.tokenize(text):
-
-                if (token not in freq_stat):
-                    freq_stat[token] = 1
-                else:
-                    freq_stat[token] += 1
-
-                if (freq_stat[token] == self.freq_bar):
-                    self.stoi[token] = idx
-                    self.itos[idx] = token
-                    idx += 1
+            freq.update(self.tokenize(text))
+        
+        self.vocab = Vocab(
+            counter=freq
+            , specials=("<pad>", "<unk>")
+            , min_freq=self.min_freq
+        )
         self.is_built = True
         return
 
@@ -76,12 +66,34 @@ class ToxicComtVocab(object):
 
         if (not self.is_built):
             raise(Exception("[ ERROR ] :: Vocabulary not built"))
-        try:
-            ret = [self.stoi.get(token, self.stoi["<UNK>"]) for token in  self.tokenize(text)]
-        except:
-            print(f"[ ERROR ] :: Tokenization failed for < {text} >")
-        return [self.stoi["<SOS>"]] + ret + [self.stoi["<EOS>"]]
-        
+
+        list_tokens = self.tokenize(text)
+        return self.vocab.lookup_indices(list_tokens)
+
+    def load_vectors(self, path_file, path_dir):
+
+        if (not self.is_built):
+            raise(Exception("[ ERROR ] :: Vocabulary not built"))
+
+        word_vec = Vectors(path_file, path_dir)
+        self.vocab.load_vectors(word_vec)
+        return
+
+    def lookup_idx(self, token):
+
+        if (not self.is_built):
+            raise(Exception("[ ERROR ] :: Vocabulary not built"))
+
+        return self.vocab.lookup_indices([token])[0]
+
+    def pad_idx(self):
+
+        return self.lookup_idx("<pad>")
+
+    def unk_idx(self):
+
+        return self.lookup_idx("<unk>")
+
 
 class ToxicComtDataset(Dataset):
 
@@ -91,7 +103,7 @@ class ToxicComtDataset(Dataset):
         #   "id"
         # Predictor/Factor:
         #   "comment_text"
-        # Labels/Targets:
+        # Labels/Targets (Binary):
         #   "toxic"
         #   "severe_toxic"
         #   "obscene"
@@ -99,32 +111,33 @@ class ToxicComtDataset(Dataset):
         #   "insult"
         #   "identity_hate"
         self.df = pd.read_csv(path, index_col="id")
-        self.factor_raw = self.df["comment_text"]
 
-        # Build a vocabulary
+        # Build vocabulary
+        list_comments = self.df.iloc[:, 0].values.tolist()
         self.vocab = ToxicComtVocab()
-        self.vocab.build_vocab(self.factor_raw)
+        self.vocab.build_vocab(list_comments)
 
-        # A temporary transformer for numericalize comments
-        def text_to_tensor(text):
-
-            ret = self.vocab.text_to_num(text)
-            ret = torch.tensor(ret, dtype=torch.long)
-            return ret
-
-        # Apply to all comments
-        self.factor = self.factor_raw.apply(text_to_tensor)
-
-        # Select just one target for binary classification
-        target_col = "toxic"
-        self.target = torch.tensor(
-            self.df.loc[:, target_col].values, dtype=torch.uint8
+        # To Torch tensors
+        self.factor = torch.tensor(
+            [self.vocab.text_to_num(t) for t in list_comments]
+            , dtype=torch.long
         )
+        self.target = torch.tensor(
+            self.df.iloc[:, 1:].values
+            , dtype=torch.float16
+        )
+        return
+
+    def get_vocab(self):
+
+        return self.vocab
 
     def __len__(self):
+
         return len(self.df)
 
     def __getitem__(self, idx):
+
         return self.factor[idx], self.target[idx]
 
 
@@ -151,36 +164,44 @@ class ToxicComtCollate(object):
 
 class ToxicComtDataLoader(object):
 
-    def __init__(
-        self
-        , path_root
-        , path_file
-        , prob_train=0.7
-    ):
+    def __init__(self, config: EasyDict):
+        self.config = config
 
-        # Get data
-        path_full = os.path.join(path_root, path_file)
-        self.dataset = ToxicComtDataset(path_full)
-
-        # Random split
-        len_train = int(prob_train * len(self.dataset))
-        len_valid = len(self.dataset) - len_train
-        dataset_train, dataset_valid = random_split(
-            self.dataset, [len_train, len_valid]
+        # Load data
+        self.dataset = ToxicComtDataset(
+            os.path.join(self.config.path_dir, self.config.path_file)
         )
 
+        # Split train test, possibly dev set
+        #   1. Create indices
+        #   2. Make samplers
+        #   3. Create separate data loaders, feeding both the datset and sampler
+        n_sample = len(self.dataset)
+        cut_train = int(self.config.pct_train * n_sample)
+        idxs_full = np.arange(n_sample)[torch.randperm(n_sample)]  # Shuffle
+
+        self.idxs_train = idxs_full[:cut_train]
+        self.idxs_valid = idxs_full[cut_train:]
+
+        splr_train = SubsetRandomSampler(self.idxs_train)
+        splr_valid = SubsetRandomSampler(self.idxs_valid)
+
         # Make loader
-        collate_fn = ToxicComtCollate(self.dataset.vocab.stoi.get("<PAD>"))
+        collate_fn = ToxicComtCollate(self.get_vocab().pad_idx())
         self.loader_train = DataLoader(
-            dataset_train
-            , batch_size=4
-            , shuffle=True
+            self.dataset
+            , sampler=splr_train
+            , batch_size=self.config.batch_size
             , collate_fn=collate_fn
         )
         self.loader_valid = DataLoader(
-            dataset_valid
-            , batch_size=4
-            , shuffle=True
+            self.dataset
+            , sampler=splr_valid
+            , batch_size=self.config.batch_size
             , collate_fn=collate_fn
         )
         return
+
+    def get_vocab(self):
+
+        return self.dataset.get_vocab()
